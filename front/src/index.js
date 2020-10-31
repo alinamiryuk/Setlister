6888d5971899c85ca729c81c66ded3fc3a5a11c1
alinamiryuk <alinamiryuk@mail.ru>
2020-10-31 21:57:51 +0300

    store, auth reducer and actions

--- front/src/index.js
+++ front/src/index.js
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { rootReducer } from '../redux/reducers/rootReducer'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  
+  <Provider store={store}>
     <App />
- ,
+  </Provider>,
   document.getElementById('root')
 )
-
-
