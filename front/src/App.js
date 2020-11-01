26c9ceae01329e14d7792bc53fb6847f32b7875c
alinamiryuk <alinamiryuk@mail.ru>
2020-11-01 22:58:35 +0300

    signup layout

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { useRoutes } from './hooks/useRoutes'
 import { useSelector } from 'react-redux'
+import { Signup } from './components/Signup/Signup'
+import { Login } from './components/Login/Login'
 
 function App() {
   const checkAuth = useSelector((state) => state.auth.success)
